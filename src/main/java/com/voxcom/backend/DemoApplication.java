c220884d740d4acfb3763db2a1d5bcaedd73234d
Midhun1618 <midhumidhun342@gmail.com>
2026-01-31 19:53:51 +0530

    Add try-catch in UserStatsResponse debug1

--- src/main/java/com/voxcom/backend/DemoApplication.java
+++ src/main/java/com/voxcom/backend/DemoApplication.java
@@ -2,6 +2,7 @@ package com.voxcom.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = "com.voxcom.backend")
 @EnableScheduling
